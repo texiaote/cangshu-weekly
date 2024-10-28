ecd737ea27c930cc9cb8a048ec9fef44d66bb95d
litieqing <litieqing01@gmail.com>
2024-10-28 21:27:50 +0800

    更新链接

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/">
+            to="/">
             最新周刊
           </Link>
         </div>
